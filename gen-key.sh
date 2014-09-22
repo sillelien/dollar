150ffd292733b47eab36fe57e3ffe28cf308d74e
Neil Ellis <neil@cazcade.com>
2014-09-22 13:14:58 +0100

    Preparing for distribution

--- gen-key.sh
+++ gen-key.sh
@@ -1,13 +1,19 @@
-[[ -d ~/.gnupg ]] || gpg --gen-key <<EOF
-1
-
-
-y
-Neil Ellis
-neil@cazcade.com
-
-O
-$0
-$0
-
+if [[ ! -d ~/.gnupg ]]
+then
+  cat >key.txt <<EOF
+%echo Generating a basic OpenPGP key
+Key-Type: DSA
+Key-Length: 1024
+Subkey-Type: ELG-E
+Subkey-Length: 1024
+Name-Real: Cazcade Limited
+Name-Comment:
+Name-Email: support@cazcade.com
+Expire-Date: 0
+Passphrase: ${GPG_PASSPHRASE}
+# Do a commit here, so that we can later print "done" :-)
+%commit
+%echo done
 EOF
+    gpg2 --batch --gen-key key.txt
+fi
