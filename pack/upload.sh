07d859d6d081720969fec87d1fd02420e05e1b8b
Neil Ellis <neil@cazcade.com>
2014-10-24 02:23:58 +0100

    deployment and tools

--- pack/upload.sh
+++ pack/upload.sh
@@ -2,5 +2,7 @@
 cd $(dirname $0)
 export BINTRAY_USER=neilellis
 export BINTRAY_KEY=${BINTRAY_API_KEY}
+
 curl -Lo /tmp/bintray-functions j.mp/bintray-functions && . /tmp/bintray-functions
+
 bint-upload-with-version neilellis dollar dollar-runtime-osx ${1} ../dist/osx/dollar.tgz
