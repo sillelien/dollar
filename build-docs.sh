a020e71575fe7f4c405c21f7c578239b8693b8fa
Neil Ellis <neil@cazcade.com>
2014-12-01 15:48:03 +0000

    Adding hexo for website

--- build-docs.sh
+++ build-docs.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -eu
 cd $(dirname $0)
+DIR=$(pwd)
 [ ! -d dist ] || rm -rf dist
 mkdir dist
 cd dist
 git clone git@github.com:neilellis/dollar.git docs
 cd docs
 git checkout gh-pages
-cp -rf ../../dollar-docs/src/main/webapp/* .
-cd ../..
+cd $DIR/dollar-docs/src/main/webapp/*
+npm install
+hexo generate
+cp -rf * $DIR/dist/docs
+cd $DIR
 mvn install exec:java -e -pl me.neilellis:dollar-docs -Dexec.mainClass="me.neilellis.dollar.docs.ParseDocs" -Dexec.args="./dist/docs"
 cd dist/docs
 git add *
