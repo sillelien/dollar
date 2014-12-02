d1434a27338863bd5a8d3ab67343e7486c97f5eb
Neil Ellis <neil@cazcade.com>
2014-12-02 13:12:37 +0000

    Preparing 0.1.0

--- pack/upload.sh
+++ pack/upload.sh
@@ -6,4 +6,4 @@ export BINTRAY_KEY=${BINTRAY_API_KEY}
 curl -Lo /tmp/bintray-functions j.mp/bintray-functions && . /tmp/bintray-functions
 
 mv ../dist/osx/dollar.tgz ../dist/osx/dollar-${1}-${2}.tgz
-bint-upload-with-version neilellis dollar dollar-runtime-osx ${1} ../dist/osx/dollar-${1}.tgz
+bint-upload-with-version neilellis dollar dollar-runtime-osx ${1} ../dist/osx/dollar-${1}-${2}.tgz
