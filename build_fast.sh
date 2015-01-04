b99285ae918909047a07f3f4575ff36034d9b5b2
Neil Ellis <neil@cazcade.com>
2015-01-04 04:10:20 +0000

    Build speedup

--- build_fast.sh
+++ build_fast.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn -T 2C -Drat.skip -Dsource.skip=true -DgenerateReports=false -Dmaven.javadoc.skip=true -Dmaven.test.skip -Denforcer.skip clean package
+mvn -T 2C -Drat.skip -Dsource.skip=true -DgenerateReports=false -Dmaven.javadoc.skip=true -Denforcer.skip clean package
