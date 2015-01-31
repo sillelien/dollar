de8a9f36cb7b501ce06611cc93674a18beadbb83
Neil Ellis <neil@cazcade.com>
2015-01-31 03:05:51 +0000

    Docker support on website

--- dollar-script/src/main/java/me/neilellis/dollar/relproxy/impl/jproxy/core/clsmgr/comp/JProxyCompilerInMemory.java
+++ dollar-script/src/main/java/me/neilellis/dollar/relproxy/impl/jproxy/core/clsmgr/comp/JProxyCompilerInMemory.java
@@ -129,8 +129,7 @@ public class JProxyCompilerInMemory {
             ClassDescriptorSourceScript sourceFileDescScript = (ClassDescriptorSourceScript) sourceFileDesc;
             LinkedList<JavaFileObject> compilationUnitsList = new LinkedList<>();
             String code = sourceFileDescScript.getSourceCode();
-            compilationUnitsList.add(
-                    new JavaFileObjectInputSourceInMemory(sourceFileDescScript.getClaREADssName(), code,
+            compilationUnitsList.add(new JavaFileObjectInputSourceInMemory(sourceFileDescScript.getClassName(), code,
                                                                            sourceFileDescScript.getEncoding(),
                                                                            sourceFileDescScript.getTimestamp()));
             compilationUnits = compilationUnitsList;
@@ -164,7 +163,6 @@ public class JProxyCompilerInMemory {
         File folderSources = engine.getFolderSources();
         if (folderSources != null) {
             finalCompilationOptions.add("-classpath");
-            finalCompilationOptions.add("-proc:none");
             finalCompilationOptions.add(engine.getFolderSources().getAbsolutePath());
         }
 
