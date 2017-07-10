984530dc14c34e5806d30e795b436ddc9b1ce4ca
neilellis <neil@vizz.buzz>
2017-07-10 19:13:27 +0100

    Bintray work

--- build-docs.sh
+++ build-docs.sh
@@ -7,13 +7,13 @@ cd dist
 git clone https://github.com/sillelien/dollar docs
 cd docs
 git checkout gh-pages
-cd $DIR/dollar-docs/src/main/webapp/
-jekyll build
+jekyll build --source $DIR/dollar-docs/src/main/webapp/ --destination .
 [ -d $DIR/dist/docs/dev ] || mkdir -p $DIR/dist/docs/dev
-cp -rf $DIR/target/staging/* $DIR/dist/docs/dev
-cp -rf * $DIR/dist/docs
 cd $DIR
-mvn -q install site exec:java -e -pl com.sillelien:dollar-docs -Dexec.mainClass="com.sillelien.dollar.docs.ParseDocs" -Dexec.args="./dist/docs"
+mvn  -Dmaven.test.skip -DskipDeploy -DstagingDirectory=$DIR/dist/docs/dev site:site site:stage
+sleep 60
+cp -rf * $DIR/dist/docs
+mvn -q install exec:java -e -pl com.sillelien:dollar-docs -Dexec.mainClass="com.sillelien.dollar.docs.ParseDocs" -Dexec.args="./dist/docs"
 cd dist/docs
 git add *
 git commit -a -m "Updated docs from build"  || :
