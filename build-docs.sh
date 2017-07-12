d99fcc35f7de2b747370dde803314cca64f2a82a
neilellis <neil@vizz.buzz>
2017-07-12 23:01:08 +0100

    Build workflow

--- build-docs.sh
+++ build-docs.sh
@@ -13,8 +13,7 @@ jekyll build --source $DIR/dollar-docs/src/main/webapp/ --destination .
 cp -rf * $DIR/dist/docs || :
 [ -d $DIR/dist/docs/dev ] || mkdir -p $DIR/dist/docs/dev
 cd $DIR
-mvn -q -Dmaven.test.skip install
-mvn -q -Dmaven.test.skip -DskipDeploy -DstagingDirectory=$DIR/dist/docs/dev/ site:site site:stage
+mvn -q -Dmaven.test.skip -DskipDeploy -DstagingDirectory=$DIR/dist/docs/dev/ install site:site site:stage
 mvn  exec:java -e -pl com.sillelien:dollar-docs -Dexec.mainClass="com.sillelien.dollar.docs.ParseDocs" -Dexec.args="./dist/docs"
 cd dist/docs
 git add *
