988676f1b31322a60164bc3e4cecf261fcb9e998
neilellis <neil@vizz.buzz>
2017-07-12 23:19:11 +0100

    Build workflow

--- deploy-dist.sh
+++ deploy-dist.sh
@@ -5,5 +5,5 @@ export BINTRAY_KEY=${BINTRAY_API_KEY}
 
 curl -Lo /tmp/bintray-functions j.mp/bintray-functions && . /tmp/bintray-functions
 
-mv ../dist/dollar.tgz ../dist/dollar-${1}-${2}.tgz
-bint-upload-with-version sillelien binary dollar ${1} ../dist/dollar-${1}-${2}.tgz
+mv dist/dollar.tgz dist/dollar-${1}-${2}.tgz
+bint-upload-with-version sillelien binary dollar ${1} dist/dollar-${1}-${2}.tgz
