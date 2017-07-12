1ffe8cb408f1b53b43cba55a4aa608c686b1a809
neilellis <neil@vizz.buzz>
2017-07-12 22:13:28 +0100

    Build workflow

---
+++ build-dist.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -eux
+cd $(dirname $0)
+PROJECT=$(pwd)
+
+[ -d dist ] && rm -rf dist || :
+DIST=dist/dollar
+
+[ -d ${DIST}/plugins ] || mkdir -p ${DIST}/plugins
+RUNTIME_JAR=$(ls ${PROJECT}/dollar-runtime/target/dollar-runtime*-mod.jar)
+
+mkdir -p ${DIST}
+
+for PLUGIN in $(ls ${PROJECT}/dollar-plugins)
+do
+    if [ -d ${PROJECT}/dollar-plugins/${PLUGIN}/target ]
+    then
+        cp -f ${PROJECT}/dollar-plugins/${PLUGIN}/target/*plugin.jar ${DIST}/plugins
+    fi
+done
+
+cp -r ${PROJECT}/dist-skel/common/* ${DIST}
+cp ${PROJECT}/LICENSE $PROJECT/README.md ${DIST}
+mkdir ${DIST}/lib
+cp ${RUNTIME_JAR} ${DIST}/lib
+
+SLF4J_VERSION=1.7.12
+LOG4J_VERSION=1.2.17
+XSTREAM_VERSION=1.4.8
+
+cp -f ~/.m2/repository/org/slf4j/slf4j-api/${SLF4J_VERSION}/slf4j-api-${SLF4J_VERSION}.jar ${DIST}/lib
+cp -f ~/.m2/repository/org/slf4j/slf4j-log4j12/${SLF4J_VERSION}/slf4j-log4j12-${SLF4J_VERSION}.jar ${DIST}/lib
+cp -f ~/.m2/repository/log4j/log4j/${LOG4J_VERSION}/log4j-${LOG4J_VERSION}.jar ${DIST}/lib
+cp -f ~/.m2/repository/com/thoughtworks/xstream/xstream/${XSTREAM_VERSION}/xstream-${XSTREAM_VERSION}.jar  ${DIST}/lib
+
+cd ${DIST}
+cd ..
+tar -zcvf dollar.tgz dollar
+
+
+
+
+
---
+++ test-dist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eux
+cd $(dirname $0)
+DIR=$(pwd)
+
+[ -d target/build_test ] || mkdir target/build_test
+cp $DIR/dollar-examples/src/main/resources/test_*.ds  target/build_test
+for file in $(ls target/build_test)
+do
+    echo "Testing: " $file
+    $DIR/dist/dollar/bin/dollar target/build_test/${file}
+done
