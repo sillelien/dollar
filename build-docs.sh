acd5b3a169f7ed4555e4f270142da71424aa7b8e
neilellis <neil@vizz.buzz>
2017-07-14 00:20:46 +0100

    GitHub API changes

--- build-docs.sh
+++ build-docs.sh
@@ -1,15 +1,1 @@
 #!/bin/bash -eux
-cd $(dirname $0)
-DIR=$(pwd)
-echo "login=neilellis" > ~/.github
-echo "password=${GITHUB_PASSWORD}" >> ~/.github
-git clone https://github.com/sillelien/dollar project
-git checkout $CIRCLE_BRANCH
-mkdir -p dist
-cd dist
-git clone https://github.com/sillelien/dollar docs
-cd docs
-git checkout gh-pages
-ls /root/dollar/project/dollar-docs/src/main/webapp/
-jekyll build --source $DIR/project/dollar-docs/src/main/webapp/ --destination $DIR/dist/docs
-cp -rf * $DIR/dist/docs || :
