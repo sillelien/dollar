09c70378b7470e7a56903d8c7793ac1a1880091f
neilellis <neil@vizz.buzz>
2017-07-14 00:10:19 +0100

    GitHub API changes

--- build-docs.sh
+++ build-docs.sh
@@ -3,7 +3,6 @@ cd $(dirname $0)
 DIR=$(pwd)
 echo "login=neilellis" > ~/.github
 echo "password=${GITHUB_PASSWORD}" >> ~/.github
-./set-version.sh
 mkdir -p dist
 cd dist
 git clone https://github.com/sillelien/dollar docs
@@ -11,13 +10,3 @@ cd docs
 git checkout gh-pages
 jekyll build --source $DIR/dollar-docs/src/main/webapp/ --destination .
 cp -rf * $DIR/dist/docs || :
-[ -d $DIR/dist/docs/dev ] || mkdir -p $DIR/dist/docs/dev
-cd $DIR
-mvn -q -Dmaven.test.skip -DskipDeploy -DstagingDirectory=$DIR/dist/docs/dev/ install site:site site:stage
-mvn  exec:java -e -pl com.sillelien:dollar-docs -Dexec.mainClass="com.sillelien.dollar.docs.ParseDocs" -Dexec.args="./dist/docs"
-cd dist/docs
-git add *
-git commit -a -m "Updated docs from build"  || :
-git push || :
-cd -
-
---
+++ build-maven-docs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -eux
+cd $(dirname $0)
+DIR=$(pwd)
+echo "login=neilellis" > ~/.github
+echo "password=${GITHUB_PASSWORD}" >> ~/.github
+./set-version.sh
+mkdir -p dist
+cd dist
+git clone https://github.com/sillelien/dollar docs
+cd docs
+git checkout gh-pages
+cp -rf * $DIR/dist/docs || :
+[ -d $DIR/dist/docs/dev ] || mkdir -p $DIR/dist/docs/dev
+cd $DIR
+mvn -q -Dmaven.test.skip -DskipDeploy -DstagingDirectory=$DIR/dist/docs/dev/ install site:site site:stage
+mvn  exec:java -e -pl com.sillelien:dollar-docs -Dexec.mainClass="com.sillelien.dollar.docs.ParseDocs" -Dexec.args="./dist/docs"
+cd dist/docs
+git add *
+git commit -a -m "Updated docs from build"  || :
+git push || :
+cd -
+
