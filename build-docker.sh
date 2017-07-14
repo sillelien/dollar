857954b4b425b2a7c141d2cfd130dcdc0a46dd27
neilellis <neil@vizz.buzz>
2017-07-14 01:48:10 +0100

    GitHub API changes

--- build-docker.sh
+++ build-docker.sh
@@ -3,6 +3,6 @@ set -eux
 cd $(dirname $0)
 DIR=$(pwd)
 RELEASE=$(cat .release)
-docker build -f Dockerfile-windows -t  sillelien-docker-docker.bintray.io/dollarscript-windows:${RELEASE:-dev}
-docker build -f Dockerfile-headless -t  sillelien-docker-docker.bintray.io/dollarscript-headless:${RELEASE:-dev}
+docker build -f $DIR/Dockerfile-windows -t  sillelien-docker-docker.bintray.io/dollarscript-windows:${RELEASE:-dev}
+docker build -f $DIR/Dockerfile-headless -t  sillelien-docker-docker.bintray.io/dollarscript-headless:${RELEASE:-dev}
 
