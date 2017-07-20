f04af9d61c8f9c914a2468b70e367615083d7c9a
neilellis <neil@vizz.buzz>
2017-07-20 02:30:12 +0100

    Camel

--- deploy-dist.sh
+++ deploy-dist.sh
@@ -2,8 +2,9 @@
 cd $(dirname $0)
 export BINTRAY_USER=neilellis
 export BINTRAY_KEY=${BINTRAY_API_KEY}
+VERSION=$(cat .release)
 
 curl -Lo /tmp/bintray-functions j.mp/bintray-functions && . /tmp/bintray-functions
 
-mv dist/dollar.tgz dist/dollar-${1}-${2}.tgz
-bint-upload-with-version sillelien binary dollar ${1} dist/dollar-${1}-${2}.tgz
+mv dist/dollar.tgz dist/dollar-${VERSION}.tgz
+bint-upload-with-version sillelien binary dollar ${VERSION} dist/dollar-${VERSION}.tgz
