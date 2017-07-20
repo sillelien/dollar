71c0c138ce363f4a3fa072d6360f1cd03c59ada4
neilellis <neil@vizz.buzz>
2017-07-20 02:52:21 +0100

    Camel

--- build-docker.sh
+++ build-docker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -eux
 cd $(dirname $0)
-DIR=$(pwd)
+PROJECT=$(pwd)
 RELEASE=$(cat .release)
 cd dist
-docker build -f $DIR/Dockerfile-windows -t  sillelien-docker-docker.bintray.io/dollarscript-windows:${RELEASE:-dev}
-docker build -f $DIR/Dockerfile-headless -t  sillelien-docker-docker.bintray.io/dollarscript-headless:${RELEASE:-dev}
+docker build -f ${PROJECT}/Dockerfile-windows -t  sillelien-docker-docker.bintray.io/dollarscript-windows:${RELEASE:-dev} .
+docker build -f ${PROJECT}/Dockerfile-headless -t  sillelien-docker-docker.bintray.io/dollarscript-headless:${RELEASE:-dev} .
 
