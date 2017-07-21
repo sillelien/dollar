d989a864d00cb0673ded5f552cd9135ab62c50a5
neilellis <neil@vizz.buzz>
2017-07-21 16:18:03 +0100

    Release and Build work

--- build-local.sh
+++ build-local.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -eu
 GITHUB_PASSWORD=$(cat ~/.github | tail -1 | cut -d= -f2)
-circleci build -e GITHUB_PASSWORD=$GITHUB_PASSWORD
+circleci build -e GITHUB_PASSWORD=$GITHUB_PASSWORD \
+               -e GIT_EMAIL=hello@neilellis.me \
+               -e GIT_NAME=neilellis \
+               -e DOCKER_EMAIL=hello@neilellis.me \
+               -e DOCKER_USER=neilellis \
+               -e DOCKER_PASS=$DOCKER_PASS \
+               -e CI_LOCAL=true
+
