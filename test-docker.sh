82ae5d1b7b0dec9043991bd72f1653cd6496d38a
neilellis <neil@vizz.buzz>
2017-07-21 17:19:45 +0100

    Release and Build work

--- test-docker.sh
+++ test-docker.sh
@@ -5,7 +5,7 @@ alias dollar=""
 [ -d /tmp/docker_test ] || mkdir /tmp/docker_test
 cp $DIR/dollar-examples/src/main/resources/test_*.ds  /tmp/docker_test
 cd /tmp/docker_test
-for file in $(ls)
+for file in $(ls | grep -v redis)
 do
     echo "Testing: " $file
     docker run -v $HOME/.github:/root/.github -v $HOME/.dollar:/root/.dollar -v $(pwd):/build -it sillelien/dollarscript-headless:${MAJOR_VERSION}  ${file}
