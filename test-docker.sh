3ea84afbda4a66d252e214cf8d8dfd52b49aef20
neilellis <neil@vizz.buzz>
2017-07-21 17:09:24 +0100

    Release and Build work

--- test-docker.sh
+++ test-docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -eux
 cd $(dirname $0)
 DIR=$(pwd)
-alias dollar="docker run -it sillelien/dollarscript-headless:${MAJOR_VERSION} -v /build:$(pwd)"
+alias dollar=""
 [ -d /tmp/docker_test ] || mkdir /tmp/docker_test
 cp $DIR/dollar-examples/src/main/resources/test_*.ds  /tmp/docker_test
 cd /tmp/docker_test
 for file in $(ls)
 do
     echo "Testing: " $file
-    dollar ${file}
+    docker run -it sillelien/dollarscript-headless:${MAJOR_VERSION} -v /build:$(pwd) ${file}
 done
 
