57c15ada62682ee142cab6ec6ea1bb3e69cae0c5
neilellis <neil@vizz.buzz>
2017-07-23 00:37:39 +0100

    Release and Build work

--- deploy-dist.sh
+++ deploy-dist.sh
@@ -6,4 +6,4 @@ VERSION=$(cat .release)
 . /home/circleci/build-utils/bintray-functions.sh
 mv dist/dollar.tgz dist/dollar-${VERSION}.tgz
 bint-upload-with-version sillelien binary dollar ${VERSION} dist/dollar-${VERSION}.tgz
-aws s3 copy dist/dollar-${VERSION}.tgz s3://dollarscript/dist/dollar-${VERSION}.tgz
+aws s3 cp dist/dollar-${VERSION}.tgz s3://dollarscript/dist/dollar-${VERSION}.tgz
