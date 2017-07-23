a27ba20d1693866ea771406ee7f546644f451211
neilellis <neil@vizz.buzz>
2017-07-23 23:43:27 +0100

    Release and Build work

--- test-docker.sh
+++ test-docker.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -eux
 cd $(dirname $0)
 DIR=$(pwd)
+RELEASE=$(cat .release)
+
 [ -d .tmp.docker_test ] || mkdir .tmp.docker_test
 cp $DIR/dollar-examples/src/main/resources/test_*.ds  .tmp.docker_test
 cd .tmp.docker_test
+
 for file in $(ls | grep -v redis)
 do
     echo "Testing: " $file
