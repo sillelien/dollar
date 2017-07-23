9944358ec42782c88d2eb9b29151d63ee79e3105
neilellis <neil@vizz.buzz>
2017-07-23 02:33:47 +0100

    Release and Build work

--- deploy-dist.sh
+++ deploy-dist.sh
@@ -6,5 +6,6 @@ VERSION=$(cat .release)
 . /home/circleci/build-utils/bintray-functions.sh
 mv dist/dollar.tgz dist/dollar-${VERSION}.tgz
 bint-upload-with-version sillelien binary dollar ${VERSION} dist/dollar-${VERSION}.tgz
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
 aws configure list
 aws s3 cp dist/dollar-${VERSION}.tgz s3://dollarscript/dist/dollar-${VERSION}.tgz
