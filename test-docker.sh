535055303693bd4f92efdebb3c003d7886dad442
neilellis <neil@vizz.buzz>
2017-07-23 13:26:17 +0100

    Release and Build work

--- test-docker.sh
+++ test-docker.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -eux
 cd $(dirname $0)
 DIR=$(pwd)
-alias dollar=""
 [ -d .tmp.docker_test ] || mkdir .tmp.docker_test
 cp $DIR/dollar-examples/src/main/resources/test_*.ds  .tmp.docker_test
 cd .tmp.docker_test
 for file in $(ls | grep -v redis)
 do
     echo "Testing: " $file
-    docker run -v $HOME/.github:/root/.github -v $HOME/.dollar:/root/.dollar -v $(pwd):/build -it sillelien/dollarscript-headless:${MAJOR_VERSION}  /build/${file}
+    docker run -v $HOME/.github:/root/.github -v $HOME/.dollar:/root/.dollar -v $(pwd):/build -it sillelien/dollarscript-headless:${RELEASE:-dev}  /build/${file}
 done
 
