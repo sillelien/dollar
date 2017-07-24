a3d7dab474d22d85c578482b1a69d670254ccfa1
neilellis <neil@vizz.buzz>
2017-07-24 19:06:27 +0100

    Docs

--- build-docker.sh
+++ build-docker.sh
@@ -2,9 +2,15 @@
 set -eux
 cd $(dirname $0)
 PROJECT=$(pwd)
-ls -l $PROJECT/dist/dollar
-
 RELEASE=$(cat .release)
+
+mkdir -p $PROJECT/dist/
+curl http://dollarscript.s3-website-eu-west-1.amazonaws.com/dist/dollar-${RELEASE}.tgz > $PROJECT/dist/dollar.tgz
+cd $PROJECT/dist
+tar -zxvf $PROJECT/dist/dollar.tgz
+ls -l dollar
+cd -
+
 cd dist
 cp ${PROJECT}/Dockerfile-windows .
 cp ${PROJECT}/Dockerfile-headless .
