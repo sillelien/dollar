876f8482393227c4d98349ac1f6694faa30da424
neilellis <neil@vizz.buzz>
2017-07-24 16:45:03 +0100

    Docs

--- build-docs-local.sh
+++ build-docs-local.sh
@@ -6,5 +6,5 @@ export RELEASE=dev
 ls $DIR/dollar-docs/src/main/webapp/
 
 envsubst < $DIR/dollar-docs/src/main/webapp/_env.yml > $DIR/dollar-docs/src/main/webapp/env.yml
-
+tail -n +2 $DIR/dollar-docs/src/main/resources/pages/scripting.md  > $DIR/dollar-docs/src/main/webapp/scripting.md
 jekyll serve --source $DIR/dollar-docs/src/main/webapp/ --config $DIR/dollar-docs/src/main/webapp/_config.yml,$DIR/dollar-docs/src/main/webapp/env.yml
--- build-docs.sh
+++ build-docs.sh
@@ -15,7 +15,7 @@ git checkout gh-pages
 
 ls /root/dollar/project/dollar-docs/src/main/webapp/
 envsubst < $DIR/project/dollar-docs/src/main/webapp/_env.yml > $DIR/project/dollar-docs/src/main/webapp/env.yml
-
+tail -n +2 $DIR/project/dollar-docs/src/main/resources/pages/scripting.md  > $DIR/project/dollar-docs/src/main/webapp/scripting.md
 jekyll build --source $DIR/project/dollar-docs/src/main/webapp/ --destination $DIR/dist/docs --config $DIR/project/dollar-docs/src/main/webapp/_config.yml,$DIR/project/dollar-docs/src/main/webapp/env.yml
 
 cp -rf * $DIR/dist/docs || :
