50976bde798ef3b80c6a06ed7a24bf98bbfb9320
neilellis <neil@vizz.buzz>
2017-07-25 00:03:15 +0100

    Project layout

--- pack/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-cd $(dirname $0)
-export BINTRAY_USER=neilellis
-export BINTRAY_KEY=${BINTRAY_API_KEY}
-
-curl -Lo /tmp/bintray-functions j.mp/bintray-functions && . /tmp/bintray-functions
-
-mv ../dist/dollar.tgz ../dist/dollar-${1}-${2}.tgz
-bint-upload-with-version sillelien binary dollar ${1} ../dist/dollar-${1}-${2}.tgz
