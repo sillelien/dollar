1fcd72623ba74a2469502bc4e34e90093ce99ab6
neilellis <neil@vizz.buzz>
2017-07-24 19:07:00 +0100

    Docs

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -5,9 +5,9 @@ DIR=$(pwd)
 RELEASE=$(cat .release)
 
 #Bintray
-docker login -u neilellis -p ${BINTRAY_API_KEY} -e hello@sillelien.com sillelien-docker-docker.bintray.io
-docker push sillelien-docker-docker.bintray.io/dollarscript-headless:${RELEASE:-dev}
-docker push sillelien-docker-docker.bintray.io/dollarscript-headless:${MAJOR_VERSION}
+#docker login -u neilellis -p ${BINTRAY_API_KEY} -e hello@sillelien.com sillelien-docker-docker.bintray.io
+#docker push sillelien-docker-docker.bintray.io/dollarscript-headless:${RELEASE:-dev}
+#docker push sillelien-docker-docker.bintray.io/dollarscript-headless:${MAJOR_VERSION}
 
 #Docker Hub
 docker login -u $DOCKER_USER -p $DOCKER_PASS
