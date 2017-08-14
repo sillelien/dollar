98eefbd35d189d25063bfdda9904eb08fb45ce55
neilellis <neil@vizz.buzz>
2017-08-14 12:35:12 +0100

    Auto generated docs

--- bin/build-devdocs.sh
+++ bin/build-devdocs.sh
@@ -3,8 +3,8 @@ set -eux
 cd $(dirname $0) && cd ..
 PROJECT=$(pwd)
 
-mkdir -p dist/devdocs
-mkdir -p dist/gendocs
+mkdir -p /home/circleci/dist/devdocs
+mkdir -p /home/circleci/dist/gendocs
 bin/set-version.sh
 mvn -q -T 2C -Dmaven.test.skip -DskipDeploy -DstagingDirectory=dist/devdocs/ install site:site site:stage
 mvn  exec:java -e -pl com.sillelien:dollar-docs -Dexec.mainClass="dollar.internal.runtime.script.parser.Symbols" > dist/gendocs/symbols.md
