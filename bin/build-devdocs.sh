5fad6a0b727dcd193c358dd2c5213b3d991c089b
neilellis <neil@vizz.buzz>
2017-08-15 16:13:21 +0100

    Build fixes

--- bin/build-devdocs.sh
+++ bin/build-devdocs.sh
@@ -4,8 +4,7 @@ cd $(dirname $0) && cd ..
 PROJECT=$(pwd)
 
 mkdir -p /home/circleci/workspace/devdocs
-mkdir -p /home/circleci/workspace/gendocs
 bin/set-version.sh
 mvn -q -T 2C -Dmaven.test.skip -DskipDeploy -DstagingDirectory=/home/circleci/workspace/devdocs/ install site:site site:stage
-mvn -q exec:java -e -Dexec.mainClass="dollar.internal.runtime.script.parser.Symbols" > /home/circleci/workspace/gendocs/symbols.md
+mvn -q  -e -Dexec.mainClass="dollar.internal.runtime.script.parser.Symbols" install exec:java > /home/circleci/workspace/gendocs/symbols.md
 
