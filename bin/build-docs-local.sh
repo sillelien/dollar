bf854bd8a26ac0d2f390ff56e2a7f51f7777dc8a
neilellis <neil@vizz.buzz>
2017-08-16 01:13:08 +0100

    New Jekyll work

--- bin/build-docs-local.sh
+++ bin/build-docs-local.sh
@@ -3,16 +3,16 @@ cd $(dirname $0) && cd ..
 PROJECT=$(pwd)
 export RELEASE=dev
 
-ls $PROJECT/dollar-docs/src/main/webapp/
+ls $PROJECT/docs/
 
-envsubst < $PROJECT/dollar-docs/src/main/webapp/_env.yml > $PROJECT/dollar-docs/src/main/webapp/env.yml
-echo "---">  $PROJECT/dollar-docs/src/main/webapp/_pages/manual.md
-echo "layout: splash" >> $PROJECT/dollar-docs/src/main/webapp/_pages/manual.md
-echo "title:  \"Dollar Scripting Language Manual\"" >> $PROJECT/dollar-docs/src/main/webapp/_pages/manual.md
-echo "permalink: /manual/" >> $PROJECT/dollar-docs/src/main/webapp/_pages/manual.md
-echo "---" >> $PROJECT/dollar-docs/src/main/webapp/_pages/manual.md
+envsubst < $PROJECT/docs/_env.yml > $PROJECT/docs/env.yml
+echo "---">  $PROJECT/docs/_pages/manual.md
+echo "layout: splash" >> $PROJECT/docs/_pages/manual.md
+echo "title:  \"Dollar Scripting Language Manual\"" >> $PROJECT/docs/_pages/manual.md
+echo "permalink: /manual/" >> $PROJECT/docs/_pages/manual.md
+echo "---" >> $PROJECT/docs/_pages/manual.md
 
-tail -n +2 $PROJECT/dollar-docs/src/main/resources/pages/manual.md  >> $PROJECT/dollar-docs/src/main/webapp/_pages/manual.md
-mvn -q  -e -Dexec.mainClass="dollar.internal.runtime.script.parser.Symbols" install exec:java >> $PROJECT/dollar-docs/src/main/webapp/_pages/manual.md || :
-cd $PROJECT/dollar-docs/src/main/webapp/
+tail -n +2 $PROJECT/dollar-docs/src/main/resources/pages/manual.md  >> $PROJECT/docs/_pages/manual.md
+mvn -q  -e -Dexec.mainClass="dollar.internal.runtime.script.parser.Symbols" install exec:java >> $PROJECT/docs/_pages/manual.md || :
+cd $PROJECT/docs/
 bundle exec jekyll serve --config _config.yml,env.yml
