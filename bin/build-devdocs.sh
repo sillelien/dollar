119a18b3feb5b6e5794f4968d39fec55f62d7e9e
neilellis <neil@vizz.buzz>
2017-08-16 01:52:48 +0100

    New Jekyll work

--- bin/build-devdocs.sh
+++ bin/build-devdocs.sh
@@ -8,5 +8,6 @@ mkdir -p /home/circleci/workspace/gendocs
 bin/set-version.sh
 mvn -q -T 2C -Dmaven.test.skip -DskipDeploy -DstagingDirectory=/home/circleci/workspace/devdocs/ install site:site site:stage
 cd dollar-script
+mvn package
 mvn -q  -e -Dexec.mainClass="dollar.internal.runtime.script.parser.Symbols" install exec:java > /home/circleci/workspace/gendocs/symbols.md
 cd -
