9b1570ea2fe1b2d7e6bb77449ad9a9b3551e192f
neilellis <neil@vizz.buzz>
2017-08-17 02:06:48 +0100

    Jekyll env file

--- bin/build-docs-local.sh
+++ bin/build-docs-local.sh
@@ -5,14 +5,15 @@ export RELEASE=dev
 
 ls $PROJECT/docs/
 
-envsubst < $PROJECT/docs/_env.yml > $PROJECT/docs/env.yml
 echo "---">  $PROJECT/docs/_pages/manual.md
 echo "layout: single" >> $PROJECT/docs/_pages/manual.md
 echo "title: \"Dollar Scripting Language Manual\"" >> $PROJECT/docs/_pages/manual.md
 echo "permalink: /manual/" >> $PROJECT/docs/_pages/manual.md
 echo "---" >> $PROJECT/docs/_pages/manual.md
-
+echo >> $PROJECT/docs/_pages/manual.md
+echo "{% include toc %}" >> $PROJECT/docs/_pages/manual.md
+echo >> $PROJECT/docs/_pages/manual.md
 tail -n +2 $PROJECT/dollar-docs/src/main/resources/pages/manual.md  >> $PROJECT/docs/_pages/manual.md
 mvn -q  -e -Dexec.mainClass="dollar.internal.runtime.script.parser.Symbols" install exec:java >> $PROJECT/docs/_pages/manual.md || :
 cd $PROJECT/docs/
-bundle exec jekyll serve --config _config.yml,env.yml
+bundle exec jekyll serve
