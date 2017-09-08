fa4524ef827cbb13e54c0f6e61f1b948a515d3ab
neilellis <neil@vizz.buzz>
2017-09-08 03:28:51 +0100

    Fixes

--- bin/test-dist.sh
+++ bin/test-dist.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0) && cd ..
 PROJECT=$(pwd)
 [ -d target/build_test ] || mkdir target/build_test
-cp $PROJECT/dollar-examples/src/main/resources/test_*.ds  target/build_test
+cp $PROJECT/dollar-examples/src/main/resources/quick/*.ds  target/build_test
 for file in $(ls target/build_test)
 do
     echo "Testing: " $file
