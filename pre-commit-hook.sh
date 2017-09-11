4916d756df0ad6130ffaa135101ee227e258658b
neilellis <neil@vizz.buzz>
2017-09-12 00:56:30 +0100

    Allowing easier variable declaration and added tests.

--- pre-commit-hook.sh
+++ pre-commit-hook.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn -q -T 1C -Drat.skip -Dsource.skip=true -DgenerateReports=false clean install &> pre-commit.log
+mvn -Drat.skip=true -Dsource.skip=true -DgenerateReports=false clean install &> pre-commit.log
